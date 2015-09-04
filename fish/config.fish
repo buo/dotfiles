3b3035acd8f6fa3f393ce63998c4c9aa2b1a0620
buo <buo@users.noreply.github.com>
2015-09-05 03:50:01 +0900

    Add DOTROOT/bin to the path list

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,6 @@ set -x DOTROOT "$HOME/.dotfiles"
 for file in (find $DOTROOT -maxdepth 2 -name "*.fish" -not -path "$DOTROOT/fish/*.fish")
   source $file
 end
+
+# Append bin to the path list.
+set PATH $PATH $DOTROOT/bin
