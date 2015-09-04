147d204f2a3f79f17f5a8721bb8a8e93a9a07ba2
buo <buo@users.noreply.github.com>
2015-09-05 05:34:16 +0900

    Add hub which is a command-line wrapper for git

---
+++ hub/hub.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+function git
+  hub $argv
+end
---
+++ hub/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# `hub` is a command-line wrapper for git that makes you better at GitHub.
+# https://hub.github.com
+
+brew install hub
