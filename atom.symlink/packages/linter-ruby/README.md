ceb1b5445b2a95e240b63eaa831d49e32f614daf
buo <buo@users.noreply.github.com>
2015-09-13 21:42:13 +0900

    atom: add linter-ruby package

---
+++ atom.symlink/packages/linter-ruby/README.md
@@ -0,0 +1,37 @@
+linter-ruby
+=========================
+
+This linter plugin for [Linter](https://github.com/AtomLinter/Linter) provides an interface to Ruby's builtin syntax analysis. It will be used with files that have the `Ruby` syntax.
+
+## Installation
+On first activation the plugin will install all dependencies automatically, you no longer have to worry about installing Linter.
+
+Just install this package and you'll be good to go.
+
+## Settings
+You can configure linter-ruby by editing ~/.atom/config.cson (choose Open Your Config in Atom menu):
+```
+'linter-ruby':
+  'rubyExecutablePath': null #ruby path. run 'which ruby' to find the path.
+  'ignoredExtensions': 'erb, md' #ignored extensions, ERB and markdown files by default.
+```
+
+## Contributing
+If you would like to contribute enhancements or fixes, please do the following:
+
+1. Fork the plugin repository.
+1. Hack on a separate topic branch created from the latest `master`.
+1. Commit and push the topic branch.
+1. Make a pull request.
+1. welcome to the club
+
+Please note that modifications should follow these coding guidelines:
+
+- Indent is 2 spaces.
+- Code should pass coffeelint linter.
+- Vertical whitespace helps readability, don’t be afraid to use it.
+
+Thank you for helping out!
+
+## Donation
+[![Share the love!](https://chewbacco-stuff.s3.amazonaws.com/donate.png)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KXUYS4ARNHCN8)
---
+++ atom.symlink/packages/linter-ruby/lib/main.js
@@ -0,0 +1,61 @@
+"use babel";
+
+export default {
+  config: {
+    rubyExecutablePath: {
+      type: "string",
+      default: "ruby"
+    },
+    ignoredExtensions: {
+      type: 'array',
+      default: ['erb', 'md'],
+      items: {
+        type: 'string'
+      }
+    }
+  },
+
+  activate: () => {
+    // We are now using steelbrain's package dependency package to install our
+    //  dependencies.
+    require("atom-package-deps").install("linter-ruby");
+  },
+
+  provideLinter: () => {
+    const helpers = require("atom-linter");
+    const Path = require("path");
+    const regex = /.+:(\d+):\s*(.+?):\s(.+)/;
+    return {
+      grammarScopes: ["source.ruby", "source.ruby.rails", "source.ruby.rspec"],
+      scope: "file",
+      lintOnFly: true,
+      lint: (activeEditor) => {
+        const command = atom.config.get("linter-ruby.rubyExecutablePath");
+        const ignored = atom.config.get("linter-ruby.ignoredExtensions");
+        const filePath = activeEditor.getPath();
+        const fileExtension = Path.extname(filePath).substr(1);
+
+        for (let extension of ignored) {
+          if (fileExtension === extension) return [];
+        }
+
+        return helpers.exec(command, ['-wc'], {stdin: activeEditor.getText(), stream: 'stderr'}).then(output => {
+          var toReturn = [];
+          output.split(/\r?\n/).forEach(function (line) {
+            const matches = regex.exec(line);
+            if (matches === null) {
+              return;
+            }
+            toReturn.push({
+              range: helpers.rangeFromLineNumber(activeEditor, Number.parseInt((matches[1] - 1))),
+              type: matches[2],
+              text: matches[3],
+              filePath: filePath
+            });
+          });
+          return toReturn;
+        });
+      }
+    };
+  }
+};
