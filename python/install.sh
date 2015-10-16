5b70a7370736280f4e3f813449116274c51a794d
buo <buo@users.noreply.github.com>
2015-10-16 13:16:00 +0900

    Check spoof-mac command already installed or not

--- python/install.sh
+++ python/install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-sudo pip install SpoofMAC
 # Install pip
 if test command -v pip >/dev/null 2>&1; then
 	curl https://bootstrap.pypa.io/get-pip.py | sudo python
+fi
+
+# Install SpoofMAC
+if test command -v spoof-mac >/dev/null 2>&1; then
+	sudo pip install SpoofMAC
 fi
