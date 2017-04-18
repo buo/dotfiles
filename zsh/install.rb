bed681115c25cbd84c77185cab223b679f924f70
buo <buo@users.noreply.github.com>
2017-04-18 10:24:47 +0900

    zsh: install.sh -> install.rb

---
+++ zsh/install.rb
@@ -0,0 +1,1 @@
+shell 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
--- zsh/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
