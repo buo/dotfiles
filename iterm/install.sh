4de200ffcb6767ac577758499322610cf61fda16
buo <buo@users.noreply.github.com>
2018-12-22 13:23:02 +0800

    Fix a bug where iTerm preferences were not applied

--- iterm/install.sh
+++ iterm/install.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+# See https://apple.stackexchange.com/a/167143
+killall iTerm2
+if [ -f "$HOME/Library/Preferences/com.googlecode.iterm2.plist" ]; then
+    defaults delete "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+fi
 cp -f "$DOTROOT/iterm/com.googlecode.iterm2.plist" "$HOME/Library/Preferences/"
+defaults read -app iTerm
