0d3c14e832c6adc776b4ee755be5a4401349e556
buo <buo@users.noreply.github.com>
2024-03-04 21:08:45 +0800

    Refactor homebrew installation script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-if test command -v brew >/dev/null 2>&1; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# Function to append a line to a file if it doesn't already exist
+append_line_to_file() {
+  local line="$1"
+  local file="$2"
+
+  if ! grep -qF "$line" "$file"; then
+    echo "$line" >>"$file"
+  else
+    echo "The line already exists in $file. Skipping..."
+  fi
+}
+
+if type -t brew &>/dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  append_line_to_file 'eval "$(/opt/homebrew/bin/brew shellenv)"' "$HOME/.zprofile"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-# Install homebrew packages
-# source $DOTROOT/homebrew/Brewfile
-
-# Install homebrew cask packages
-# source $DOTROOT/homebrew/Caskfile
 
 brew cleanup
