3c33b0dc5fb323f611c2f0b878bd4ffed647974f
buo <buo@users.noreply.github.com>
2024-03-04 21:11:51 +0800

    Remove obsolete installation scripts and files
    Deleted installation scripts for Alfred, iTerm, Python, and PlantUML as they are no longer needed.

--- alfred/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -d "/Applications/Alfred 4.app" ]; then
-  echo "Alfred 4.app already installed."
-else
-  brew cask instal alfred
-fi
--- iterm/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -d "/Applications/iTerm.app" ]; then
-  echo "iTerm.app already installed."
-else
-  brew cask instal iterm2
-fi
